e93d61f6bfa87e3bb6f930a10e24df4e4fa774c4
yday05506 <yangdayeon2@gmail.com>
2023-05-25 09:57:30 +0900

    정석적인 피보나치 수열

---
+++ RecursionPrac/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RecursionPrac
+{
+    internal class Fibonacci
+    {
+        public long Get(int n)
+        {
+            // Get(3) -> Get(2) + Get(1)
+            if (n < 0) { return 0; }
+            if (n == 1) { return 1; }
+            return Get(n-2) + Get(n-1);
+        }
+        
+    }
+}
--- RecursionPrac/Program.cs
+++ RecursionPrac/Program.cs
@@ -10,6 +10,14 @@ namespace RecursionPrac
     {
         static void Main(string[] args)
         {
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(1));
+            Console.WriteLine(fibo.Get(2));
+            Console.WriteLine(fibo.Get(3));
+            Console.WriteLine(fibo.Get(4));
+            Console.WriteLine(fibo.Get(5));
+            Console.WriteLine(fibo.Get(6));
+            Console.WriteLine(fibo.Get(7));
         }
     }
 }
